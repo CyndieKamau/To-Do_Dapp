8eca9ff5e234e11f8d5225603e7848e3df1196e1
Cynthia Mwihaki Kamau <cyndiekamaa@gmail.com>
2023-04-12 19:21:17 +0300

    2. Writing the todo list smart contract- intro

--- contracts/ToDoList.sol
+++ contracts/ToDoList.sol
@@ -4,6 +4,32 @@ pragma solidity ^0.5.0;
 
 contract ToDoList {
     //add the task counts as a state variable
-    uint public taskcount = 0;
+    uint public task_count = 0;
+
+    //define the structure of our to do list
+    struct tasks_structure {
+        uint task_id;   //an unsigned int.
+        string task_to_do;
+        bool completed;
+    }
+
+    //Store our tasks in blockchain; key is an uint id, value tasks_structure
+    mapping(uint => tasks_structure) public tasks_map;
+
+
+    //constructor fn for when smart contract is deployed first time
+    constructor() public {
+        tasks_structure_mapping("This is a default task to finish when you deploy this smart contract the first time.");
+    }
+
+
+    //add tasks_list to mapping
+    function tasks_structure_mapping(string memory _task_to_do) public {
+        task_count ++;
+        tasks_map[task_count] = tasks_structure(task_count, _task_to_do, false);
+
+
+    }
+
 
 }
