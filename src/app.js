495eefd44acd0560666ffc4a941ec8452df69e0f
Cynthia Mwihaki Kamau <cyndiekamaa@gmail.com>
2023-04-13 17:23:30 +0300

    4. Added Javascript

---
+++ src/app.js
@@ -0,0 +1,37 @@
+App = {
+    load: async() => {
+        await App.loadWeb3()
+    },
+}
+
+//https-medium-com-metamask-breaking-change-injecting-web3-7722797916a8
+loadWeb3: async () => {
+    if (typeof web3 !== 'undefined') {
+      App.web3Provider = web3.currentProvider
+      web3 = new Web3(web3.currentProvider)
+    } else {
+      window.alert("Please connect to Metamask.")
+    }
+    // Modern dapp browsers...
+    if (window.ethereum) {
+        window.web3 = new Web3(ethereum);
+        try {
+            // Request account access if needed
+            await ethereum.enable();
+            // Acccounts now exposed
+            web3.eth.sendTransaction({/* ... */});
+        } catch (error) {
+            // User denied account access...
+        }
+    }
+    // Legacy dapp browsers...
+    else if (window.web3) {
+        window.web3 = new Web3(web3.currentProvider);
+        // Acccounts always exposed
+        web3.eth.sendTransaction({/* ... */});
+    }
+    // Non-dapp browsers...
+    else {
+        console.log('Non-Ethereum browser detected. You should consider trying MetaMask!');
+    }
+};
